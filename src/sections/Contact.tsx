d853dff57127a555cda23d985b88b7381902a06f
cindellai <cindellai26@gmail.com>
2024-10-23 15:09:36 -0500

    "Added Footer section, updated Contact section's background gradient, and modified Hero section's animation"

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -5,7 +5,7 @@ export const ContactSection = () => {
   return (
   <div className='py-16 pt-12 lg:py-24 lg:pt-20'>
     <div className='container'>
-      <div className='bg-gradient-to-r from-pink-300 to-purple-300 text-gray-900 py-8 px-10 rounded-3xl text-center md:text-left relative overflow-hidden z-0'> 
+      <div className='bg-gradient-to-r from-pink-400 to-purple-300 text-gray-900 py-8 px-10 rounded-3xl text-center md:text-left relative overflow-hidden z-0'> 
       <div className='absolute inset-0 opacity-5 -z-10' style={{backgroundImage: `url(${grainImage})`
       }} >
       </div>
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -57,7 +57,7 @@ export const HeroSection = () => {
         <div className="flex flex-col items-center">
         <Image src={memojiImage} className="size-[130px]" alt="Person looking over laptop" />
         <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg">
-        <div className="bg-green-500 size-2.5 rounded-full"></div>
+        <div className="bg-green-500 size-2.5 rounded-full animate-ping"></div>
 
       {/* Text Section */}
       <div className="text-sm font-meduim">Available for new projects</div>
