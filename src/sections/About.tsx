dc525a16632f41d8a8ac6198efa83bafebb4aa57
cindellai <cindellai26@gmail.com>
2024-10-22 19:03:13 -0500

    "Added AboutSection component and updated ProjectsSection to use Card component"

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,3 +1,18 @@
+import { SectionHeader } from "@/components/SectionHeader";
+import { Card } from "@/components/Card";
+import StarIcon from "@/assets/icons/star.svg";
+
 export const AboutSection = () => {
-  return <div>About Section</div>;
+  return (
+    <div className="pb-96">
+      <SectionHeader eyebrow="About Me" title="A Glimpse Into My World" description="Learn more about who I a, what I do, and what inspires me" />
+    <div>
+      <Card>
+        <div>
+          <StarIcon />
+        </div>
+      </Card>
+    </div>
+    </div>
+  );
 };
