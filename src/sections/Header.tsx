537050e0f5ffab35a7d2a5657ba2ae111e136589
cindellai <cindellai26@gmail.com>
2024-10-29 19:51:00 -0500

    "Updated Link components in Header.tsx to include className and scroll props, and removed nested a tags."

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -4,17 +4,17 @@ export const Header = () => {
   return (
     <div className="flex justify-center items-center fixed top-3 w-full z-10">
       <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full backdrop-blur">
-        <Link href="#home" scroll={false}>
-          <a className="nav-item">Home</a>
+        <Link href="#home" scroll={true} className="nav-item">
+          Home
         </Link>
-        <Link href="#projects" scroll={false}>
-          <a className="nav-item">Projects</a>
+        <Link href="#projects" scroll={true} className="nav-item">
+          Projects
         </Link>
-        <Link href="#about" scroll={false}>
-          <a className="nav-item">About</a>
+        <Link href="#about" scroll={true} className="nav-item">
+          About
         </Link>
-        <Link href="#contact" scroll={false}>
-          <a className="nav-item bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900">Contact</a>
+        <Link href="#contact" scroll={true} className="nav-item bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900">
+          Contact
         </Link>
       </nav>
     </div>
